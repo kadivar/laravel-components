1589b54fb52a8981ff95e7dcba14bfe966f3f663
Mohammadreza Kadivar <me.kadivar@gmail.com>
2019-10-15 12:26:20 +0330

    Autoload requests. Fix stubs loading.

--- src/ComponentsServiceProvider.php
+++ src/ComponentsServiceProvider.php
@@ -5,81 +5,90 @@ namespace Kadivar\Components;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
 
-class ComponentsServiceProvider extends ServiceProvider {
+class ComponentsServiceProvider extends ServiceProvider
+{
 
-	protected $files;
+    protected $files;
 
-	/**
-	 * Bootstrap the application services.
-	 *
-	 * @return void
-	 */
-	public function boot() {
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if (is_dir(app_path().'/Components/')) {
+            $components = config("components.enable") ?: array_map('class_basename',
+                $this->files->directories(app_path().'/Components/'));
+            foreach ($components as $component) {
+                // Allow routes to be cached
+                $helper = app_path().'/Components/'.$component.'/helper.php';
+                $trans = app_path().'/Components/'.$component.'/Translations';
+                $views = app_path().'/Components/'.$component.'/Views';
+                $requests = app_path().'/Components/'.$component.'/Requests';
+                $routes = app_path().'/Components/'.$component.'/Routes';
+                $controllers = app_path().'/Components/'.$component.'/Controllers';
 
-		if ( is_dir( app_path() . '/Components/' ) ) {
-			$components = config( "components.enable" ) ?: array_map( 'class_basename', $this->files->directories( app_path() . '/Components/' ) );
-			foreach ( $components as $component ) {
-				// Allow routes to be cached
-				$helper      = app_path() . '/Components/' . $component . '/helper.php';
-				$controllers = app_path() . '/Components/' . $component . '/Controllers';
-				$routes      = app_path() . '/Components/' . $component . '/Routes';
-				$views       = app_path() . '/Components/' . $component . '/Views';
-				$trans       = app_path() . '/Components/' . $component . '/Translations';
+                if ($this->files->exists($helper)) {
+                    include $helper;
+                }
+                if ($this->files->isDirectory($trans)) {
+                    $this->loadTranslationsFrom($trans, $component);
+                }
+                if ($this->files->isDirectory($views)) {
+                    $this->loadViewsFrom($views, $component);
+                }
+                if ($this->files->isDirectory($requests)) {
+                    foreach (glob($requests.'/*.php') as $filename) {
+                        include $filename;
+                    }
+                }
+                if ($this->files->isDirectory($routes)) {
+                    if (!$this->app->routesAreCached()) {
+                        if ($this->files->exists($routes.'/web.php')) {
+                            include $routes.'/web.php';
+                        }
+                        if ($this->files->exists($routes.'/api.php')) {
+                            include $routes.'/api.php';
+                        }
+                    }
+                }
+                if ($this->files->isDirectory($controllers)) {
+                    foreach (glob($controllers.'/*.php') as $filename) {
+                        include $filename;
+                    }
+                }
+            }
+        }
+    }
 
-				if ( $this->files->exists( $helper ) ) {
-					include $helper;
-				}
-				if ( $this->files->isDirectory( $controllers ) ) {
-					foreach ( glob( $controllers . '/*.php' ) as $filename ) {
-						include $filename;
-					}
-				}
-				if ( $this->files->isDirectory( $routes ) ) {
-					if ( ! $this->app->routesAreCached() ) {
-						if ( $this->files->exists( $routes . '/web.php' ) ) {
-							include $routes . '/web.php';
-						}
-						if ( $this->files->exists( $routes . '/api.php' ) ) {
-							include $routes . '/api.php';
-						}
-					}
-				}
-				if ( $this->files->isDirectory( $views ) ) {
-					$this->loadViewsFrom( $views, $component );
-				}
-				if ( $this->files->isDirectory( $trans ) ) {
-					$this->loadTranslationsFrom( $trans, $component );
-				}
-			}
-		}
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
 
-	}
+        $this->files = new Filesystem;
+        $this->registerMakeCommand();
+    }
 
-	/**
-	 * Register the application services.
-	 *
-	 * @return void
-	 */
-	public function register() {
+    /**
+     * Register the "make:component" console command.
+     *
+     * @return Console\ComponentsMakeCommand
+     */
+    protected function registerMakeCommand()
+    {
 
-		$this->files = new Filesystem;
-		$this->registerMakeCommand();
-	}
+        $this->commands('components.make');
 
-	/**
-	 * Register the "make:component" console command.
-	 *
-	 * @return Console\ComponentsMakeCommand
-	 */
-	protected function registerMakeCommand() {
+        $bind_method = method_exists($this->app, 'bindShared') ? 'bindShared' : 'singleton';
 
-		$this->commands( 'components.make' );
-
-		$bind_method = method_exists( $this->app, 'bindShared' ) ? 'bindShared' : 'singleton';
-
-		$this->app->{$bind_method}( 'components.make', function ( $app ) {
-			return new Console\ComponentsMakeCommand( $this->files );
-		} );
-	}
+        $this->app->{$bind_method}('components.make', function ($app) {
+            return new Console\ComponentsMakeCommand($this->files);
+        });
+    }
 
 }
--- src/Console/ComponentsMakeCommand.php
+++ src/Console/ComponentsMakeCommand.php
@@ -7,235 +7,247 @@ use Illuminate\Support\Str;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
-class ComponentsMakeCommand extends GeneratorCommand {
+class ComponentsMakeCommand extends GeneratorCommand
+{
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'make:component';
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:component';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Create a new component (folder structure)';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new component (folder structure)';
 
-	/**
-	 * The type of class being generated.
-	 *
-	 * @var string
-	 */
-	protected $type = 'Component';
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Component';
 
-	/**
-	 * The current stub.
-	 *
-	 * @var string
-	 */
-	protected $currentStub;
-
-
-	/**
-	 * Execute the console command.
-	 *
-	 * @return void
-	 */
-	public function handle() {
-		// check if component exists
-		if ( $this->files->exists( app_path() . '/Components/' . $this->getNameInput() ) ) {
-			return $this->error( $this->type . ' already exists!' );
-		}
-
-		// Create Controller
-		$this->generate( 'controller' );
-
-		// Create Model
-		$this->generate( 'model' );
-
-		// Create Views folder
-		$this->generate( 'view' );
-
-		//Flag for no translation
-		if ( $this->option( 'translation' ) ) // Create Translations folder
-		{
-			$this->generate( 'translation' );
-		}
-
-		// Create Routes file
-		$this->generate( 'route' );
-
-		// Create Helper file
-		$this->generate( 'helper' );
-
-		// Create Request Folder
-		$this->generate( 'request' );
+    /**
+     * The current stub.
+     *
+     * @var string
+     */
+    protected $currentStub;
 
 
-		if ( $this->option( 'migration' ) ) {
-			$table = Str::plural( Str::snake( class_basename( $this->argument( 'name' ) ) ) );
-			$this->call( 'make:migration', [ 'name' => "create_{$table}_table", '--create' => $table ] );
-		}
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // check if component exists
+        if ($this->files->exists(app_path().'/Components/'.$this->getNameInput())) {
+            return $this->error($this->type.' already exists!');
+        }
 
-		$this->info( $this->type . ' created successfully.' );
-	}
+        // Create Controller
+        $this->generate('controller');
+
+        // Create Model
+        $this->generate('model');
+
+        // Create Views folder
+        $this->generate('view');
+
+        //Flag for no translation
+        if ($this->option('translation')) // Create Translations folder
+        {
+            $this->generate('translation');
+        }
+
+        // Create Routes file
+        $this->generate('route');
+
+        // Create Helper file
+        $this->generate('helper');
+
+        // Create Request Folder
+        $this->generate('request');
 
 
-	protected function generate( $type ) {
+        if ($this->option('migration')) {
+            $table = Str::plural(Str::snake(class_basename($this->argument('name'))));
+            $this->call('make:migration', ['name' => "create_{$table}_table", '--create' => $table]);
+        }
 
-		switch ( $type ) {
-			case 'controller':
-				$file_name = Str::studly( class_basename( $this->getNameInput() ) . ucfirst( $type ) );
-				break;
+        $this->info($this->type.' created successfully.');
+    }
 
-			case 'model':
-				$file_name = Str::studly( class_basename( $this->getNameInput() ) );
-				break;
 
-			case 'view':
-				$file_name = 'index.blade';
-				break;
+    protected function generate($type)
+    {
 
-			case 'translation':
-				$file_name = 'en';
-				break;
+        switch ($type) {
+            case 'controller':
+                $file_name = Str::studly(class_basename($this->getNameInput()).ucfirst($type));
+                break;
 
-			case 'route':
-				$file_name = 'routes';
-				break;
+            case 'model':
+                $file_name = Str::studly(class_basename($this->getNameInput()));
+                break;
 
-			case 'helper':
-				$file_name = 'helper';
-				break;
-			case 'request':
-				$file_name = Str::studly( class_basename( $this->getNameInput() ) . ucfirst( $type ) );
-				break;
-		}
+            case 'view':
+                $file_name = 'index.blade';
+                break;
 
-		$folder = ( $type != 'helper' ) ? ucfirst( $type ) . 's\\' . ( $type === 'translation' ? 'en\\' : '' ) : '';
-		$name   = $this->getFileClassNamespace( $folder, $file_name );
-		$path   = $this->getPath( $name );
+            case 'translation':
+                $file_name = 'en';
+                break;
 
-		if ( $this->files->exists( $path ) ) {
-			return $this->error( $this->type . ' already exists!' );
-		}
+            case 'route':
+                $file_name = 'routes';
+                break;
 
-		$this->makeDirectory( $path );
+            case 'helper':
+                $file_name = 'helper';
+                break;
+            case 'request':
+                $file_name = Str::studly(class_basename($this->getNameInput()).ucfirst($type));
+                break;
+        }
 
-		if ( $type == 'route' ) {
-			$name              = $this->getFileClassNamespace( $folder, 'web' );
-			$path              = $this->getPath( $name );
-			$this->currentStub = __DIR__ . '/stubs/web.stub';
-			$this->files->put( $path, $this->buildClass( $name ) );
-			$name              = $this->getFileClassNamespace( $folder, 'api' );
-			$path              = $this->getPath( $name );
-			$this->currentStub = __DIR__ . '/stubs/api.stub';
-			$this->files->put( $path, $this->buildClass( $name ) );
-		} else {
-			$this->currentStub = __DIR__ . '/stubs/api.stub';
-			$this->files->put( $path, $this->buildClass( $name ) );
-		}
-	}
+        $folder = ($type != 'helper') ? ucfirst($type).'s\\'.($type === 'translation' ? 'en\\' : '') : '';
+        $name = $this->getFileClassNamespace($folder, $file_name);
+        $path = $this->getPath($name);
 
-	/**
-	 * Get the class full namespace as string.
-	 *
-	 * @param  string $folder
-	 * @param  string $file_name
-	 *
-	 * @return string
-	 */
-	protected function getFileClassNamespace( $folder, $file_name ) {
-		$name = $name = (string) ( 'Components\\' . Str::studly( ucfirst( $this->getNameInput() ) ) . '\\' . $folder . $file_name );
+        if ($this->files->exists($path)) {
+            return $this->error($this->type.' already exists!');
+        }
 
-		return $name;
-	}
+        $this->makeDirectory($path);
 
-	/**
-	 * Build the class with the given name.
-	 *
-	 * @param  string $name
-	 *
-	 * @return string
-	 */
-	protected function buildClass( $name ) {
-		$stub = $this->files->get( $this->getStub() );
+        if ($type == 'route') {
+            $name = $this->getFileClassNamespace($folder, 'web');
+            $path = $this->getPath($name);
+            $this->currentStub = __DIR__.'/stubs/web.stub';
+            $this->files->put($path, $this->buildClass($name));
+            $name = $this->getFileClassNamespace($folder, 'api');
+            $path = $this->getPath($name);
+            $this->currentStub = __DIR__.'/stubs/api.stub';
+            $this->files->put($path, $this->buildClass($name));
+        } else {
+            $this->currentStub = __DIR__.'/stubs/'.$type.'.stub';
+            $this->files->put($path, $this->buildClass($name));
+        }
+    }
 
-		return $this->replaceName( $stub, $this->getNameInput() )->replaceNamespace( $stub, $name )->replaceClass( $stub, $name );
-	}
+    /**
+     * Get the class full namespace as string.
+     *
+     * @param  string  $folder
+     * @param  string  $file_name
+     *
+     * @return string
+     */
+    protected function getFileClassNamespace($folder, $file_name)
+    {
+        $name = $name = (string) ('Components\\'.Str::studly(ucfirst($this->getNameInput())).'\\'.$folder.$file_name);
 
-	/**
-	 * Get the stub file for the generator.
-	 *
-	 * @return string
-	 */
-	protected function getStub() {
-		return $this->currentStub;
-	}
+        return $name;
+    }
 
-	/**
-	 * Replace the name for the given stub.
-	 *
-	 * @param  string $stub
-	 * @param  string $name
-	 *
-	 * @return string
-	 */
-	protected function replaceName( &$stub, $name ) {
-		$stub = str_replace( 'DummyTitle', $name, $stub );
-		$stub = str_replace( 'DummyUCtitle', ucfirst( Str::studly( $name ) ), $stub );
+    /**
+     * Build the class with the given name.
+     *
+     * @param  string  $name
+     *
+     * @return string
+     */
+    protected function buildClass($name)
+    {
+        $stub = $this->files->get($this->getStub());
 
-		return $this;
-	}
+        return $this->replaceName($stub, $this->getNameInput())->replaceNamespace($stub, $name)->replaceClass($stub,
+            $name);
+    }
 
-	/**
-	 * Get the full namespace name for a given class.
-	 *
-	 * @param  string $name
-	 *
-	 * @return string
-	 */
-	protected function getNamespace( $name ) {
-		return trim( implode( '\\', array_map( 'ucfirst', array_slice( explode( '\\', Str::studly( $name ) ), 0, - 1 ) ) ), '\\' );
-	}
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return $this->currentStub;
+    }
 
-	/**
-	 * Replace the class name for the given stub.
-	 *
-	 * @param  string $stub
-	 * @param  string $name
-	 *
-	 * @return string
-	 */
-	protected function replaceClass( $stub, $name ) {
-		$class = class_basename( $name );
+    /**
+     * Replace the name for the given stub.
+     *
+     * @param  string  $stub
+     * @param  string  $name
+     *
+     * @return string
+     */
+    protected function replaceName(&$stub, $name)
+    {
+        $stub = str_replace('DummyTitle', $name, $stub);
+        $stub = str_replace('DummyUCtitle', ucfirst(Str::studly($name)), $stub);
 
-		return str_replace( 'DummyClass', $class, $stub );
-	}
+        return $this;
+    }
 
-	/**
-	 * Get the console command arguments.
-	 *
-	 * @return array
-	 */
-	protected function getArguments() {
-		return array(
-			[ 'name', InputArgument::REQUIRED, 'Component name.' ],
-		);
-	}
+    /**
+     * Get the full namespace name for a given class.
+     *
+     * @param  string  $name
+     *
+     * @return string
+     */
+    protected function getNamespace($name)
+    {
+        return trim(implode('\\', array_map('ucfirst', array_slice(explode('\\', Str::studly($name)), 0, -1))), '\\');
+    }
 
-	/**
-	 * Get the console command options.
-	 *
-	 * @return array
-	 */
-	protected function getOptions() {
-		return array(
-			[ 'migration', null, InputOption::VALUE_NONE, 'Create new migration files.' ],
-			[ 'translation', null, InputOption::VALUE_NONE, 'Create component translation filesystem.' ],
-		);
-	}
+    /**
+     * Replace the class name for the given stub.
+     *
+     * @param  string  $stub
+     * @param  string  $name
+     *
+     * @return string
+     */
+    protected function replaceClass($stub, $name)
+    {
+        $class = class_basename($name);
+
+        return str_replace('DummyClass', $class, $stub);
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return array(
+            ['name', InputArgument::REQUIRED, 'Component name.'],
+        );
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return array(
+            ['migration', null, InputOption::VALUE_NONE, 'Create new migration files.'],
+            ['translation', null, InputOption::VALUE_NONE, 'Create component translation filesystem.'],
+        );
+    }
 
 }
